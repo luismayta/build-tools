c578bacd64e49d0c281e1d75698503d82156f82a
@slovacus <slovacus@gmail.com>
2021-05-16 18:03:56 -0500

    chore: implement generator and template for plop (#8)

---
+++ provision/generators/plop/generators/validate.ts
@@ -0,0 +1,37 @@
+import { Actions, PlopGeneratorConfig } from 'node-plop'
+import * as path from 'path'
+import { ValidatePrompNames, AnswersValidate as Answers } from './entities'
+import { baseRootPath, baseTemplatesPath, pathExists } from '../utils'
+import { sanitize } from '../helpers'
+const testPath = path.join(baseRootPath, 'test')
+
+export const validateGenerator: PlopGeneratorConfig = {
+  description: 'add an test validate',
+  prompts: [
+    {
+      type: 'input',
+      name: ValidatePrompNames.name,
+      message: 'What should it be program to validate?',
+      default: 'terraform'
+    }
+  ],
+  actions: (data) => {
+    const answers = data as Answers
+    const validatePath = `${testPath}/validate/`
+
+    if (pathExists(validatePath)) {
+      throw new Error(`Stage '${answers.nameValidate}' exists in '${validatePath}'`)
+    }
+
+    const actions: Actions = []
+
+    actions.push({
+      type: 'append',
+      templateFile: `${baseTemplatesPath}/validate/test.append.hbs`,
+      path: `${testPath}/${sanitize(answers.nameValidate)}_test.go`,
+      abortOnFail: true
+    })
+
+    return actions
+  }
+}
