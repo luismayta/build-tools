1118c43a358e5fef8c23ad76b6be5136a3a8d518
Luis Mayta <luis@hadenlabs.com>
2021-05-16 19:28:40 -0500

    refactor: build name to builder (#8)

--- provision/generators/plop/generators/builder.ts
+++ provision/generators/plop/generators/builder.ts
@@ -1,16 +1,16 @@
 import { Actions, PlopGeneratorConfig } from 'node-plop'
 import * as path from 'path'
-import { BuildPrompNames, AnswersBuild as Answers } from './entities'
+import { BuilderPrompNames, AnswersBuilder as Answers } from './entities'
 import { baseRootPath, baseTemplatesPath, pathExists } from '../utils'
-const testPath = path.join(baseRootPath, 'test', 'build')
+const testPath = path.join(baseRootPath, 'test', 'builder')
 
-export const buildGenerator: PlopGeneratorConfig = {
-  description: 'add a test build',
+export const builderGenerator: PlopGeneratorConfig = {
+  description: 'add a test builder',
   prompts: [
     {
       type: 'input',
-      name: BuildPrompNames.name,
-      message: 'What should it be name build?',
+      name: BuilderPrompNames.name,
+      message: 'What should it be name builder?',
       default: 'docker'
     }
   ],
@@ -18,14 +18,14 @@ export const buildGenerator: PlopGeneratorConfig = {
     const answers = data as Answers
 
     if (!pathExists(testPath)) {
-      throw new Error(`path '${answers.nameBuild}' not exists in '${testPath}'`)
+      throw new Error(`path '${answers.nameBuilder}' not exists in '${testPath}'`)
     }
 
     const actions: Actions = []
 
     actions.push({
       type: 'append',
-      templateFile: `${baseTemplatesPath}/build/test.append.hbs`,
+      templateFile: `${baseTemplatesPath}/builder/test.append.hbs`,
       path: `${testPath}/docker_test.go`,
       abortOnFail: true
     })
